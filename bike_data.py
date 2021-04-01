18f58eddd14bde58f056c044bc600e69383feb86
Luca Franceschini <luca.france@outlook.com>
2021-04-01 17:16:21 +0200

    API-query updated to download all available data

--- bike_data.py
+++ bike_data.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     filename="bike-traffic-munich.log", 
     format="%(asctime)s %(levelname)s %(message)s", 
     encoding="utf-8", 
-    level=logging.INFO)
+    level=logging.DEBUG)
 
 
 def reset_directories():
@@ -21,16 +21,36 @@ def reset_directories():
     
     
 def download_csv():
-    r = requests.get("https://www.opengov-muenchen.de/api/3/action/package_search?q=Raddauerz%C3%A4hlstellen")
-    for result in r.json()["result"]["results"]:
-        for resource in result["resources"]:
-            csv_url = resource["url"]
-            csv_name = csv_url.split("/")[-1]
-            csv_path = os.path.join("csv", csv_name)
-            r_csv = requests.get(csv_url)
-            with open(csv_path, "wb") as f:
-                f.write(r_csv.content)
-            logging.info("Saved \"{}\"".format(csv_path))
+    
+    # Find all urls of the csv files to download. 
+    months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "October", "November", "Dezember"]
+    queries = ["Raddauerzählstellen München {} {}".format(month, year) for year in range(2017, 2022) for month in months]
+    csv_urls = []
+    for query in queries:
+        r = requests.get("https://www.opengov-muenchen.de/api/3/action/package_search", params = {"q": query})
+        query_result = r.json()["result"]
+        logging.debug("The query for {} returned {} results.".format(query, query_result["count"]))
+        for result in query_result["results"]:
+            for resource in result["resources"]:
+                res_name = resource["name"]
+                if query in res_name:
+                    logging.info("Found resource \"{}\" in the results of query \"{}\".".format(res_name, query))
+                    csv_urls.append(resource["url"])
+                else:
+                    logging.debug("Ignored resource \"{}\" in the results of query \"{}\".".format(res_name, query))
+    
+    # Link to the csv of the resource "Raddauerzählstellen in München"
+    # https://www.opengov-muenchen.de/dataset/raddauerzaehlstellen-muenchen/resource/211e882d-fadd-468a-bf8a-0014ae65a393
+    csv_urls.append("https://www.opengov-muenchen.de/dataset/aca4bcb6-d0ff-4634-b5b9-8b5d133ab08e/resource/211e882d-fadd-468a-bf8a-0014ae65a393/download/radzaehlstellen.csv")
+                
+    # Download the csv files to the "csv" folder
+    for csv_url in csv_urls:
+        csv_name = csv_url.split("/")[-1]
+        csv_path = os.path.join("csv", csv_name)
+        r_csv = requests.get(csv_url)
+        with open(csv_path, "wb") as f:
+            f.write(r_csv.content)
+        logging.info("Saved \"{}\"".format(csv_path))
     return
 
 
@@ -56,6 +76,8 @@ def build_dataset():
     csv_15min_concat = pd.concat(csv_data_15min,ignore_index=True)
     logging.info("Dataframes concatenated.")
     
+    #TODO Datum format is incosistent in July 2019: DD.MM.YYYY instead of YYYY.MM.TT 
+    
     sort_clms = ["datum", "uhrzeit_start", "zaehlstelle"]
     csv_day_concat.sort_values(sort_clms)
     csv_15min_concat.sort_values(sort_clms)
@@ -66,8 +88,6 @@ def build_dataset():
     logging.info("Dataframes saved to csv.")
     
     return
-    
-    
     
     
 if __name__ == "__main__":
