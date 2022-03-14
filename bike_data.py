193ddc69530d4a7ef99e1f863fa3a1e9030a5537
Luca Franceschini <luca.france@outlook.com>
2022-03-14 10:46:02 +0100

    Update to include 2022 data

--- bike_data.py
+++ bike_data.py
@@ -27,7 +27,7 @@ def download_csv():
     
     # Find all urls of the csv files to download. 
     months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
-    queries = ["Raddauerzählstellen München {} {}".format(month, year) for year in range(2017, 2022) for month in months]
+    queries = ["Raddauerzählstellen München {} {}".format(month, year) for year in range(2017, 2023) for month in months]
     csv_urls = []
     for query in queries:
         r = requests.get("https://www.opengov-muenchen.de/api/3/action/package_search", params = {"q": query})
